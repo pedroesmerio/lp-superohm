7280f51ab7ba1715a274ab9094cefe0f2c01d5e6
pedroesmerio <p.h.esmerio@gmail.com>
2021-05-31 19:43:33 -0300

    :tada: First Config commit

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  images: {
+    domains: ['lp.superohm.com.br'],
+  },
+};
---
+++ src/pages/camara-climatica-para-calibracao-de-sensores.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/cccali.module.scss';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello Dev</h1>
+    </div>
+  );
+}
